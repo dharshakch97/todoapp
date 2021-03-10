fe000a164328ccf0b088a54a6bd2160372c02b5e
Dharshak Ch <dharshak.ch97@gmail.com>
2021-03-10 17:16:30 +0530

    todo-item, toggle functionality

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+
+  @Input() todo: Todo
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // set dynamic classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-completed': this.todo.completed
+    }
+    return classes
+  }
+
+  onToggle(todo: any) {
+    todo.completed = !todo.completed
+  }
+
+  onDelete(todo: any) {
+    console.log('delete')
+  }
+}
