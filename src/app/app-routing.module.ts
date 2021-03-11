27f33feef42654c0f8eac9af4606460493460a72
Dharshak Ch <dharshak.ch97@gmail.com>
2021-03-11 12:53:08 +0530

    creating a new branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TodosComponent } from './components/todos/todos.component';
 
-const routes: Routes = [
-  { path: '', component: TodosComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
